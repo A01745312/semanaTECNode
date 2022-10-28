fcacd5e10c196247ad1d4de07ad615c70043bdbb
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-28 11:03:44 -0500

    Cambio

--- utils/database.js
+++ utils/database.js
@@ -4,8 +4,8 @@ const Sequelize = require('sequelize')
 
 // Objeto de conexion
 
-const sequelize = new Sequelize (process.env.DB, process.env.USER, process.env.PASSWORD, {
-    dialect: process.env.DIALECT,
+const sequelize = new Sequelize ('semanatec', 'admin', 'Sapa11_15', {
+    dialect: 'mysql',
     host: process.env.HOST,
     define: {
         timestamps: false,
