945d6ad67f7f7a6a5c7340fed47724435a57238e
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-28 10:11:59 -0500

    Cors

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 // Librerias
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const fileUpload = require('express-fileupload')
 const modeloIARoutes = require('./routes/modeloIA')
 const ejemploBDRoutes = require('./routes/ejemploBD')
 const sequelize = require('./utils/database')
 
+
 // Crea servidor
 const app = express();
 
 // Middlewares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
+
 
 // Cargar las rutas
 app.use(modeloIARoutes);
@@ -33,6 +37,3 @@ sequelize.sync()
         })
     })
     .catch (error => console.log(error))
-
-
-
