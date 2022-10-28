ad21634c26cb21c72b875a252a01e6dfdc6e6271
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-28 11:04:42 -0500

    Cambio1

--- utils/database.js
+++ utils/database.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize ('semanatec', 'admin', 'Sapa11_15', {
     dialect: 'mysql',
-    host: process.env.HOST,
+    host: 'teamtaiko.cs2l0gbpepod.us-east-1.rds.amazonaws.com',
     define: {
         timestamps: false,
         freezeTableName: true
